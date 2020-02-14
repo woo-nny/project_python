9bc5c4d1df03fe8d3d8973ef7ba4e6792c46cb7a
KIMYONGCHEOL <skekdnl12@gmail.com>
2020-02-14 14:49:27 +0900

    pushpushbaby

--- Dragon_SnakeGame.py
+++ Dragon_SnakeGame.py
@@ -26,14 +26,18 @@ class Snake:
         self.position.append([X,Y])
 
     def die(self):
-        head = self.position[0]
-        for i in self.position[1:]:
-            if head == i:
-                return True
+        if self.position[0] in self.position[1:] or (snake.position[0][0] < 100 or snake.position[0][0] > 350 
+        or snake.position[0][1] < 100 or snake.position[0][1] > 350):
+            textRect = text.get_rect()
+            textRect.center = (ScreenSize[0] // 2, ScreenSize[1] // 2)
+            return screen.blit(text,textRect)
 
-
-
+class Feed:
+    def __init__(self):
+        self.feed_X = feed_X
+        self.feed_Y = feed_Y
         
+
 # 마우스 클릭에 따라 어느 방향으로 얼마만큼 이동할지 판단해주는 함수.
 def movement(X,Y,a,b):
     if a >= X and abs(a - X) >= abs(b - Y):
@@ -74,7 +78,7 @@ sec_interval = timedelta(seconds=1)
 
 
 
-while True:  
+while snake.die() == None:  
     Change_Flag = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
@@ -85,25 +89,20 @@ while True:
             cha_direction = movement(snake.position[0][0],snake.position[0][1],a,b)
             Change_Flag = True
     
-    if Change_Flag == True:
-        snake.direction = cha_direction
-        snake.automove()
 
     if Interval < datetime.now() - lasttime:
         feed_X = random.randint(100,350)
         feed_Y = random.randint(100,350)
         lasttime = datetime.now()
-    if datetime.now() - sec > sec_interval:
+    if datetime.now() - sec > sec_interval or Change_Flag:
+        snake.direction = cha_direction
         snake.automove()
         sec = datetime.now()
+        
     if feed_X - 10 <= snake.position[0][0] <= feed_X + 10 and feed_Y - 10 <= snake.position[0][1] <= feed_Y + 10:
         feed_X = random.randint(100,350)
         feed_Y = random.randint(100,350)
         snake.grow()
-    
-    if snake.die == True:
-        print("game over")
-        break
 
     
 
@@ -112,10 +111,7 @@ while True:
     for X,Y in snake.position:
         pygame.draw.rect(screen, green, [X,Y,10,10])
     pygame.draw.rect(screen, black, [feed_X,feed_Y,10,10])
-    if snake.position[0][0] < 100 or snake.position[0][0] > 350 or snake.position[0][1] < 100 or snake.position[0][1] > 350:
-        textRect = text.get_rect()
-        textRect.center = (ScreenSize[0] // 2, ScreenSize[1] // 2)
-        screen.blit(text,textRect)
+    snake.die()
     
             
     pygame.display.flip()
