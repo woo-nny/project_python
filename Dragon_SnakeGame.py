9db4122ef598e2ffe43e5b3cf67da4b780778a84
KIMYONGCHEOL <skekdnl12@gmail.com>
2020-02-19 15:20:27 +0900

    Newgame까지 가능

--- Dragon_SnakeGame.py
+++ Dragon_SnakeGame.py
@@ -33,9 +33,9 @@ class Snake:
             self.direction = "under"
     # 먹이를 먹어 커지는 경우
     def grow(self):
-        if (feed.feed_X - 10 <= snake.position[0][0] <= feed.feed_X + 10 
-        and feed.feed_Y - 10 <= snake.position[0][1] <= feed.feed_Y + 10):
-            feed.change_position()
+        if (gameboard.feed.feed_X - 10 <= gameboard.snake.position[0][0] <= gameboard.feed.feed_X + 10 
+        and gameboard.feed.feed_Y - 10 <= gameboard.snake.position[0][1] <= gameboard.feed.feed_Y + 10):
+            gameboard.feed.change_position()
             X,Y = self.position[-1]
             if self.direction == "right":
                 self.position.append([X-10,Y])
@@ -47,11 +47,15 @@ class Snake:
                 self.position.append([X,Y+10])
     # 죽는 경우
     def die(self):
-        if self.position[0] in self.position[1:] or (snake.position[0][0] < 100 or snake.position[0][0] > 350 
-        or snake.position[0][1] < 100 or snake.position[0][1] > 350):
-            textRect = text.get_rect()
-            textRect.center = (ScreenSize[0] // 2, ScreenSize[1] // 2)
-            return screen.blit(text,textRect)
+        if self.position[0] in self.position[1:] or (gameboard.snake.position[0][0] < 100 or gameboard.snake.position[0][0] > 350 
+        or gameboard.snake.position[0][1] < 100 or gameboard.snake.position[0][1] > 350):
+            return "die"
+        else:
+            return None
+    
+    def reset(self):
+        self.position = [[130,130]]
+        self.direction = "under"
 
 class Feed:
     def __init__(self):
@@ -70,7 +74,89 @@ class get_click:
         self.X = a
         self.Y = b
 
+class Game_board:
 
+    def __init__(self):
+        self.snake = Snake()
+        self.feed = Feed()
+        
+    def die(self):
+        global state_snake_die
+        global state_feed_die
+        if self.snake.die() is not None:
+            if len(state_snake_die)==0:
+                for X, Y in self.snake.position:
+                    state_snake_die.append([X,Y])
+                state_feed_die = [self.feed.feed_X,self.feed.feed_Y]
+            return "die"
+        else:
+            return None
+
+    
+    def move(self, X, Y):
+        self.snake.movement(X,Y)
+        self.snake.automove()
+    
+    def grow(self):
+        self.snake.grow()
+
+    def Time(self,Interval,lasttime,sec, X, Y):
+        if Interval < datetime.now() - lasttime:
+            self.feed.feed_X, self.feed.feed_Y = random.randint(100,350), random.randint(100,350)
+            lasttime = datetime.now()
+        if datetime.now() - sec > sec_interval or Change_Flag:
+            gameboard.move(X,Y)
+            sec = datetime.now()
+        return lasttime,sec
+# 죽었을때와, 죽지 않았을때를 나눠서 출력하게 만듦.
+    def draw(self,die_commend):
+        global state_snake_die
+        global state_feed_die
+        if die_commend == None:
+            screen.fill(white)
+            pygame.draw.lines(screen, red, True,[(100,100),(100,350),(350,350),(350,100)],5)
+            for X,Y in self.snake.position:
+                pygame.draw.rect(screen, green, [X,Y,10,10])
+            pygame.draw.rect(screen, black, [gameboard.feed.feed_X,gameboard.feed.feed_Y,10,10])
+            pygame.display.flip()
+        else:
+            button = Button()
+            screen.fill(white)
+            pygame.draw.lines(screen, red, True,[(100,100),(100,350),(350,350),(350,100)],5)
+            for X,Y in state_snake_die:
+                pygame.draw.rect(screen, green, [X,Y,10,10])
+            pygame.draw.rect(screen, black, [state_feed_die[0],state_feed_die[1],10,10])
+            button.gameover()
+            button.retry()
+            pygame.display.flip()
+    def Newgame(self):
+        global state_feed_die
+        global state_snake_die
+        state_feed_die = []
+        state_snake_die = []
+
+# 게임에서 졋을때 나올 버튼들을 만드는 장소
+class Button:
+    
+    def gameover(self):
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        overtext = font.render('Game Over', True, green, blue)
+        overtextRect = overtext.get_rect()
+        overtextRect.center = (ScreenSize[0] // 2, ScreenSize[1] // 2)
+        return screen.blit(overtext,overtextRect)
+    def retry(self):
+        font = pygame.font.Font('freesansbold.ttf', 16)
+        retrytext = font.render('화면을 클릭하면 retry',True,green,blue)
+        retrytextRect = retrytext.get_rect()
+        retrytextRect.center = (ScreenSize[0] // 2 + 16, ScreenSize[1] // 2 + 64)
+        return screen.blit(retrytext,retrytextRect)
+    
+    def click_retry(self,die_com,Change_Flag):
+        if die_com != None and Change_Flag == True:
+            global gameboard
+            gameboard.Newgame()
+            gameboard = Game_board()
+                
 
 
 pygame.init()   
@@ -85,21 +171,23 @@ red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
 
-font = pygame.font.Font('freesansbold.ttf', 32)
-text = font.render('Game Over', True, green, blue)
-
-snake = Snake()
-feed = Feed()
+#보드에 나올 객체 및 클릭좌표, 죽었을 때 인식할 좌표 저장공간.
+gameboard = Game_board()
+button = Button()
 click = get_click()
+state_snake_die = []
+state_feed_die = []
+die_com = None
 
+#시간관리 영역
 Interval = timedelta(seconds=10)
 lasttime = datetime.now()
 sec = datetime.now()
 sec_interval = timedelta(seconds=1)
 
 
-pygame.K_PAUSE
-while snake.die() == None:  
+
+while True:
     Change_Flag = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
@@ -108,24 +196,12 @@ while snake.die() == None:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click.getpoint()
             Change_Flag = True
+    button.click_retry(die_com,Change_Flag)
+    lasttime,sec = gameboard.Time(Interval,lasttime,sec,click.X,click.Y)
+    gameboard.grow()
+    die_com = gameboard.die()
+    gameboard.draw(die_com)
+
     
-
-    if Interval < datetime.now() - lasttime:
-        feed.feed_X, feed.feed_Y = random.randint(100,350), random.randint(100,350)
-        lasttime = datetime.now()
-    if datetime.now() - sec > sec_interval or Change_Flag:
-        snake.movement(click.X,click.Y)
-        snake.automove()
-        sec = datetime.now()
-    snake.grow()
-
-
-    screen.fill(white)
-    pygame.draw.lines(screen, red, True,[(100,100),(100,350),(350,350),(350,100)],5)
-    for X,Y in snake.position:
-        pygame.draw.rect(screen, green, [X,Y,10,10])
-    pygame.draw.rect(screen, black, [feed.feed_X,feed.feed_Y,10,10])
-    snake.die()
     
-            
-    pygame.display.flip()
+    
